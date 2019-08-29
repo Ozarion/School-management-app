237d2c3d448e5fa79633fb45f9fcacf743a5ccf0
root <areeba.aic@gmail.com>
2019-08-29 15:17:34 +0500

    added tests for admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'parents/new'
   get 'student/new'
   root 'users#show'
-   get 'teacher/new'
-   resources :teacher
-   resources :student
-   resources :parents
+  get 'teacher/new'
+  resources :teacher
+  resources :student
+  resources :parents
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+user = User.create(name: 'Admin', email: 'admin@app.com', password: 'admin123', password_confirmation: 'admin123')
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
